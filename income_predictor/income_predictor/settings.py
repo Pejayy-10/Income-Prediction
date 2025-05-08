2ee4e2a862665953d39903f5b23c1f9579bb3d01
Pejay <frandilbertperuso@gmail.com>
2025-05-08 18:03:32 +0800

    Bootstrap to Tailwind transition

--- income_predictor/income_predictor/settings.py
+++ income_predictor/income_predictor/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'predictor',
+    'tailwind',
+    'theme',
+    'django_browser_reload',
+    'compressor',
 ]
 
 MIDDLEWARE = [
